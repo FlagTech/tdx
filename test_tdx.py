dbfcc200431dc03e4fc8d8befd3b90a139963fd2
meebox <meebox@gmail.com>
2022-06-30 15:13:11 +0800

    add comments

--- test_tdx.py
+++ test_tdx.py
@@ -8,8 +8,9 @@ tdx = TDX(client_id, client_secret)
 
 # url = 'https://tdx.transportdata.tw/api/basic/v2/Rail/TRA/LiveBoard/Station/1000?$filter=Direction eq 1&$format=JSON'
 base_url = "https://tdx.transportdata.tw/api"
-# 取得指定[車站]列車即時到離站電子看板(動態前後30分鐘的車次)
+# 取得指定[車站]列車即時到離站電子看板 (動態前後 30 分鐘的車次)
 endpoint = "/basic/v2/Rail/TRA/LiveBoard/Station/1000"
+# MicroPython 的 get 不會自動 urlencode, 要自己做
 filter = "Direction%20eq%201"  # 順逆行: [0:'順行', 1:'逆行']
 url = f"{base_url}{endpoint}?$filter={filter}&$format=JSON"
 
@@ -19,7 +20,7 @@ for station in stations:
     print(f'{station["StationName"]["Zh_tw"]}->{station["EndingStationName"]["Zh_tw"]}')
 time.sleep(3)
 print('3 secconds later:')
-# 取得指定[車站]列車即時到離站電子看板(動態前後30分鐘的車次)
+# 3 後改以嘉義車站查詢, 測試不需重新索取 token
 endpoint = "/basic/v2/Rail/TRA/LiveBoard/Station/4080"
 url = f"{base_url}{endpoint}?$filter={filter}&$format=JSON"
 stations = tdx.get_json(url)
